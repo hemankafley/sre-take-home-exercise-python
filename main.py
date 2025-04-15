23090870a6f3cb6450fef38ed7bbf62df6a5ca3b
HaytiKafley <kafleyh@miamioh.edu>
2025-04-15 17:13:19 -0400

    Updated main.py with all the new changes and comments, also added availiability.log to see all the logs in new file.

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import yaml
 import requests
 import time
+import logging   # Imported logging for file logging 
 from collections import defaultdict
+from urllib.parse import urlparse  #Imported URLPARSE to correctly extract domain without the port number
+
+
+# Configure logging to write results to a file (availability.log)
+logging.basicConfig(filename='availability.log', level=logging.INFO, format='%(asctime)s - %(message)s')
+
 
 # Function to load configuration from the YAML file
 def load_config(file_path):
@@ -11,13 +18,16 @@ def load_config(file_path):
 # Function to perform health checks
 def check_health(endpoint):
     url = endpoint['url']
-    method = endpoint.get('method')
-    headers = endpoint.get('headers')
+    method = endpoint.get('method', 'GET').upper() # Make the Default to GET method if not provided
+    headers = endpoint.get('headers', {})  #Used empty headers if not provided
     body = endpoint.get('body')
 
     try:
-        response = requests.request(method, url, headers=headers, json=body)
-        if 200 <= response.status_code < 300:
+        start = time.time()  #start timing the request
+        response = requests.request(method, url, headers=headers, json=body, timeout=0.5) #Added 0.5s timeout per requirements
+        duration = (time.time() - start) * 1000  # we can convert this to ms
+
+        if 200 <= response.status_code < 300 and duration <= 500: #Endpoint is UP only if status is between 200 and 299 AND duration is <= 500ms
             return "UP"
         else:
             return "DOWN"
@@ -31,7 +41,7 @@ def monitor_endpoints(file_path):
 
     while True:
         for endpoint in config:
-            domain = endpoint["url"].split("//")[-1].split("/")[0]
+            domain = urlparse(endpoint["url"]).hostname # We can ignore port numbers when calculating domain
             result = check_health(endpoint)
 
             domain_stats[domain]["total"] += 1
@@ -41,10 +51,12 @@ def monitor_endpoints(file_path):
         # Log cumulative availability percentages
         for domain, stats in domain_stats.items():
             availability = round(100 * stats["up"] / stats["total"])
-            print(f"{domain} has {availability}% availability percentage")
+            message = f"{domain} has {availability}% availability"
+            print(message) #prints to terminal
+            logging.info(message) #logs the result to logging file (availibility.log)
 
         print("---")
-        time.sleep(15)
+        time.sleep(15) #runs every 15 sec as required
 
 # Entry point of the program
 if __name__ == "__main__":
